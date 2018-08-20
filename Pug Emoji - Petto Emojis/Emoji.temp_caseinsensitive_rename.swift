cc00676c1aeb8caf6bc97ab3f3191dc4031496a9
Steve C <scasica0@csu.fullerton.edu>
2018-08-20 11:28:06 -0700

    Add files via upload

---
+++ Pug Emoji - Petto Emojis/Emoji.temp_caseinsensitive_rename.swift
@@ -0,0 +1,17 @@
+//
+//  emoji.swift
+//  Pug Emoji - Petto Emojis
+//
+//  Created by steve on 8/8/17.
+//
+//
+
+import Foundation
+
+class Emoji {
+    var name:String?
+    
+    init (name:String, group: String){
+        self.name = name
+    }
+}
